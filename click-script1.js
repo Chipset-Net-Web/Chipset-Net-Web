d106e80c13be314bff1e3c6613ae8c830b9a7fe8
Chipset-Net-Web <93904450+Chipset-Net-Web@users.noreply.github.com>
2021-11-11 13:40:26 +0200

    Add files via upload

---
+++ click-script1.js
@@ -0,0 +1,10 @@
+//10.11.2021. ChipsetNetWeb 
+
+
+
+let a = document.querySelector('img');
+
+a.onclick = function() {
+    document.location.href = "https://chipset-net-web.github.io/chipset-net-web-video-chat-1/";
+
+}
