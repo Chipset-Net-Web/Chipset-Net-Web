722c6de5a22a0af67bd01bfa3d4e6f7e52e83769
Chipset-Net-Web <93904450+Chipset-Net-Web@users.noreply.github.com>
2021-11-12 13:30:43 +0200

    Update click-script-2.js

--- click-script-2.js
+++ click-script-2.js
@@ -2,7 +2,7 @@
 
 
 
-let a = document.getElementById('img');
+let a = document.getElementById('text file');
 
 a.onclick = function() {
     document.location.href = "https://chipset-net-web.github.io/text-file-chat-3/";
